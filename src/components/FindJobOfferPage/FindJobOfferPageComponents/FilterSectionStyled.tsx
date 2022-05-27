e4f3eb5d13d4f4c7d58f976ad0ff16b7699e5c98
Damian Cygan <damiancygan@damiancygan.local>
2022-05-27 10:56:42 +0200

    styling changes in nav and filters

--- src/components/FindJobOfferPage/FindJobOfferPageComponents/FilterSectionStyled.tsx
+++ src/components/FindJobOfferPage/FindJobOfferPageComponents/FilterSectionStyled.tsx
@@ -1,39 +1,40 @@
 import styled from "styled-components";
 
 export const FilterTailWrapper = styled.div`
-    width: 90vw;
-    padding: 0.5rem 4rem;
+  width: 90vw;
+  padding: 0.5rem 4rem;
+  display: flex;
+  background-color: #ffffff;
+  margin: 15px auto 0;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 0px rgb(0 0 0 / 40%);
+  position: relative;
+
+  .search-input-wrapper {
+    width: 25%;
+    min-width: 150px;
+    margin-right: 20px;
+    background-color: #e9e9e9;
+    border-radius: 20px;
     display: flex;
-    background-color: #ffffff;
-    margin: 15px auto 0;
-    border-radius: 10px; 
-    box-shadow: 0px 0px 5px 0px rgb(0 0 0 / 40%);
-
-    .search-input-wrapper{
-        width: 25%;
-        margin-right: 20px;
-        background-color: #e9e9e9;
-        border-radius: 20px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        input{
-            width: 80%;
-            height: 35px;
-            padding-left: 5px;
-            outline: none;
-            border: none;
-            background-color: #e9e9e9;
-        }
+    justify-content: center;
+    align-items: center;
+    padding: 0px 5px;
+    input {
+      width: 80%;
+      height: 35px;
+      padding-left: 5px;
+      outline: none;
+      border: none;
+      background: none;
     }
+  }
 
-    select{
-        width: 18%;
-        height: 35px;
-        border: 1px solid #bcbcbc;
-        padding: 0 5px;
-        outline: none;
-        border-radius: 20px;
-        margin-right: 20px;
-    }
+  select {
+    padding: 10px 16px;
+    border-radius: 20px;
+    border: 0;
+    outline: 1px solid #bcbcbc;
+    border-right: 10px solid transparent;
+  }
 `;
--- src/components/ReusedComponents/Navbar/NavbarComponents/NavbarOptionsStyled.tsx
+++ src/components/ReusedComponents/Navbar/NavbarComponents/NavbarOptionsStyled.tsx
@@ -1,49 +1,49 @@
 import styled from "styled-components";
 
 export const NavbarOptionsWrapper = styled.div`
-    width: 55vw;
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
 
-    ul{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    li{
-        padding: 0.1rem 1rem;
-        margin: 2px 0.8rem;
-        list-style-type: none;
-        font-size: 1.1rem;
-        font-weight: 500;
-        cursor: pointer;
-    }
-    li:hover{
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    li {
+      padding: 0.1rem 1rem;
+      margin: 2px 0.8rem;
+      list-style-type: none;
+      font-size: 1.1rem;
+      font-weight: 500;
+      cursor: pointer;
+      transition: all 0.3s;
+      &:hover {
         transform: scale(1.05);
         color: #594076;
-        font-weight: 600;
+      }
     }
+  }
 
-    .navbar-access-btns-wrapper{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .navbar-access-btns-wrapper> button{
-        height: 35px;
-        background-color: #9671d6;
-        box-sizing: border-box;
-        margin: 2px 10px;
-        padding: 8px 0.5rem 8px 1rem;
-        border-radius: 20px;
-        font-size: 0.9rem;
-        outline: none;
-        border: none;
-        color: #ffffff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-    }
+  .navbar-access-btns-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .navbar-access-btns-wrapper > button {
+    height: 35px;
+    background-color: #9671d6;
+    box-sizing: border-box;
+    margin: 2px 10px;
+    padding: 8px 0.5rem 8px 1rem;
+    border-radius: 20px;
+    font-size: 0.9rem;
+    outline: none;
+    border: none;
+    color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
 `;
