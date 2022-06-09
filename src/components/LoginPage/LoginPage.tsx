17f70f9c1c25e16197703575bbda74e2c048ffec
My Name <66422260+Radek25@users.noreply.github.com>
2022-06-09 21:13:57 +0200

    Done LoginPage

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -4,12 +4,44 @@ import Image from "next/image";
 /* -- Components imports -- */
 import { LoginPageWrapper } from "./LoginPageStyled";
 
-import IMG from "../../imgs/LoginPageIMG.png"
+/* -- IMG and Icons imports -- */
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import GoogleIcon from '@mui/icons-material/Google';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export const LoginPage : FC = () => {
     return(
         <LoginPageWrapper>
-            <Image src={IMG} alt=""/>
+            <div className="circle-wrapper">
+                <h2>Pierwszy raz?</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    <br/>Perferendis ipsum dolor sit amet consectetur, adipisicing. 
+                    <br/>Perferendis,  itaque nisi recusandae natus sit vitae, magni nulla!
+                    <br/>Perferendis ipsum dolor sit amet consectetur, adipisicing.
+                </p>
+                <button>REJESTRACJA</button>
+            </div>
+            <div className="login-form-wrapper">
+                <form>
+                    <span>
+                        <PersonOutlineIcon fontSize="large" className="user-icon"/>
+                        <input type="email" placeholder="Email"/>
+                    </span>
+                    <span>
+                        <LockOutlinedIcon  fontSize="large" className="password-icon"/>
+                        <input type="password" placeholder="Password"/>
+                    </span>
+                    <button type="submit">Zaloguj się</button>
+                </form>
+                <div className="social-media-icons-wrapper">
+                    <a><FacebookOutlinedIcon fontSize="large"/></a>
+                    <a><GoogleIcon fontSize="large"/></a>
+                    <a><LinkedInIcon fontSize="large"/></a>
+                </div>
+            </div>
         </LoginPageWrapper>
     );
 };
--- src/components/LoginPage/LoginPageStyled.tsx
+++ src/components/LoginPage/LoginPageStyled.tsx
@@ -5,15 +5,100 @@ export const LoginPageWrapper = styled.div`
     background-color: white;
     height: 100vh;
     position: relative;
-    .circle, .content{
-        width: 40%;
-        height: 100%;
+    display: flex;
+    .circle-wrapper, .login-form-wrapper{
+        height: 100vh;
     }
-    .circle{
+    .circle-wrapper{
+        width: 50vw;
+        clip-path: ellipse(86% 100% at 13% 11%);
         background-color: #a39cf6;
-        border-radius: 0 0 50% 0;
+        text-align: center;
+        padding: 5vh 5%;
+        color: #ffffff;
+        h2{
+            font-size: 3rem;
+            font-family: 'Patrick Hand', cursive;
+            margin-bottom: 5px;
+        }
+        p{
+            letter-spacing: 0.02rem;
+        }
+        button{
+            width: 200px;
+            height: 50px;
+            margin: 20px 0 5px;
+            border-radius: 40px;
+            background-color: transparent;
+            color: #ffffff;
+            border: 2px solid #ffffff;
+            padding: 5px;
+            font-weight: 600;
+            font-size: 1rem;
+            letter-spacing: 0.03rem;
+            font-family: 'Open Sans', sans-serif;
+        }
     }
-    .content{
-
+    .login-form-wrapper{
+        width: 50vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        form{
+            width: 65%;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            span{
+                width: 100%;
+                height: 60px;
+                margin: 10px 5px;
+                padding: 2px 20px;
+                background-color: #ebebeb;
+                border-radius: 50px;
+                display: flex;
+                align-items: center;
+                .user-icon, .password-icon{
+                    width: auto;
+                    color: #929292;
+                }
+                input{
+                    width: 100%;
+                    height: 60px;
+                    border-radius: 0 50px 50px 0;
+                    padding: 5px 2px 5px 15px;
+                    font-size: 1.2rem;
+                    font-weight: 600;
+                    border: none;
+                    background-color: transparent;
+                    outline: none;
+                    color: #606060;
+                }
+            }
+            button{
+                width: 210px;
+                height: 50px;
+                margin: 10px auto 15px;
+                border-radius: 50px;
+                border: none;
+                background-color: #a39cf6;
+                font-family: 'Open Sans', sans-serif;
+                font-size: 1.2rem;
+                font-weight: 700;
+                color: #ffffff;
+            }
+        }
+        .social-media-icons-wrapper{
+            width: 150px;
+            min-width: 120px;
+            margin: 10px 2px 5px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            a{
+                cursor: pointer;
+            }
+        }
     }
 `;
